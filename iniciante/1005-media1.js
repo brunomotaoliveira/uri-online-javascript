ef8a61deecbfd4e5bd5378dba16134c3bf0d76b1
brunomotadev <brunomotadev@gmail.com>
2021-07-04 21:28:29 -0300

    1005 - Media Aritmética Ponderada

---
+++ iniciante/1005-media1.js
@@ -0,0 +1,14 @@
+const lines = prompt("Digite os valores").split(' ');
+
+let a = parseFloat(lines[0]);
+let b = parseFloat(lines[1]);
+
+media = ((a * 3.5) + (b * 7.5))/11;
+
+console.log("MEDIA = " + media.toFixed(5));
+
+/**
+ *  Média Aritmética Ponderada
+    A média aritmética ponderada é calculada multiplicando cada valor do conjunto de dados pelo seu peso.
+    Depois, encontra-se a soma desses valores que será dividida pela soma dos pesos.
+ */
