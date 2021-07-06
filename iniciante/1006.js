2f45740bba0d683a264883b6f361314e286fab4b
brunomotadev <brunomotadev@gmail.com>
2021-07-06 19:11:51 -0300

    adicionado comentario na media aritmetica ponderada

--- iniciante/1006.js
+++ iniciante/1006.js
@@ -8,3 +8,8 @@ let media = ((a * 2) + (b * 3) + (c * 5))/10;
 
 console.log("MEDIA = " + media.toFixed(1));
 
+/**
+ *  Média Aritmética Ponderada
+    A média aritmética ponderada é calculada multiplicando cada valor do conjunto de dados pelo seu peso.
+    Depois, encontra-se a soma desses valores que será dividida pela soma dos pesos.
+ */
