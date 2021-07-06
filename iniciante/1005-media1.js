9c389cd0e8e9bf5ca61be292b1711811d6341c5a
brunomotadev <brunomotadev@gmail.com>
2021-07-06 19:11:27 -0300

    Media 2 - Aritmetica Ponderada

--- iniciante/1005-media1.js
+++ iniciante/1005-media1.js
@@ -3,7 +3,7 @@ const lines = prompt("Digite os valores").split(' ');
 let a = parseFloat(lines[0]);
 let b = parseFloat(lines[1]);
 
-media = ((a * 3.5) + (b * 7.5))/11;
+let media = ((a * 3.5) + (b * 7.5))/11;
 
 console.log("MEDIA = " + media.toFixed(5));
 
---
+++ iniciante/1006.js
@@ -0,0 +1,10 @@
+const lines = prompt("Digite os valores:").split(' ');
+
+let a = parseFloat(lines[0]);
+let b = parseFloat(lines[1]);
+let c = parseFloat(lines[2]);
+
+let media = ((a * 2) + (b * 3) + (c * 5))/10;
+
+console.log("MEDIA = " + media.toFixed(1));
+
